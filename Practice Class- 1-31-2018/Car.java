887b44634ff33a40824d58d4c7326096c6db6835
Anthony Edwards <anthonydwrds8@gmail.com>
2018-01-31 14:35:19 -0500

    Create car.java

---
+++ Practice Class- 1-31-2018/Car.java
@@ -0,0 +1,14 @@
+package springlab;
+
+public class Car extends Car_test{
+		String carBrand;
+		double price;
+		int year;
+
+	public Car (String carBrand2,double price2, int year2) {
+		carBrand=carBrand2;
+		price=price2;
+		year=year2;
+	}
+
+}
