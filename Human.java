12899cacf14393989aea38a64c080a13a5403871
Anthony Edwards <anthonydwrds8@gmail.com>
2018-02-01 14:11:33 -0500

    Practice Class 02/01/2018
    Stuff

---
+++ Human.java
@@ -0,0 +1,17 @@
+package springlab.SpringLabs;
+
+public class Human {
+	String Firstname;
+	String Lastname;
+	String MidIntial;
+	String career;
+	int yearsOld;
+	public Human(String Fname, String Lname, String MI, String cr, int age) {
+	Firstname=Fname;
+	Lastname=Lname;
+	MidIntial=MI;
+	career=cr;
+	yearsOld=age;
+	}
+
+}
---
+++ Human_test.java
@@ -0,0 +1,20 @@
+package springlab.SpringLabs;
+
+import java.util.Scanner;
+
+public class Human_test {
+
+	public static void main(String[] args) {
+ Scanner person= new Scanner(System.in);
+String Fname = person.nextLine();
+String Lname = person.nextLine();
+String MI = person.nextLine();
+String cr = person.nextLine();
+person.next();
+int age =person.nextInt();
+Human anthony= new Human(Fname,Lname,MI,cr,age);
+person.close();
+System.out.println(anthony.Firstname);
+	}
+
+}
