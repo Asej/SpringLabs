2d4f0bac0b448248ded4f816986b4f125f4ad1b6
Anthony Edwards <anthonydwrds8@gmail.com>
2018-02-06 14:37:01 -0500

    Add files via upload

---
+++ Practice class 02-06-2018/County.java
@@ -0,0 +1,18 @@
+package springlab.SpringLabs;
+
+public class County {
+
+		String CountryName;
+		int CountryPopulation;
+		int CountryYearEstablished;
+		int CountryLandVoulume;
+
+	public County (String Name, int Population, int YearEstablished,int LandVoulume) {
+		// TODO Auto-generated method stub
+		CountryName=Name;
+		CountryPopulation=Population;
+		CountryYearEstablished=YearEstablished;
+		CountryLandVoulume=LandVoulume;
+	}
+}
+
