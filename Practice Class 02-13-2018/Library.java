782a0723dbc667859b400789afe74a4dc9fa4d0f
Anthony Edwards <anthonydwrds8@gmail.com>
2018-02-13 14:09:33 -0500

    Add files via upload

---
+++ Practice Class 02-13-2018/Library.java
@@ -0,0 +1,17 @@
+package springlab.SpringLabs;
+
+public class Library {
+	String Location;
+	int Books;
+	int Staff;
+	double expenses;
+
+	public Library(String library_location, int Total_Books, int library_staff, double Yearly_expenses ) {
+		Location=library_location;
+		Books=Total_Books;
+		Staff=library_staff;
+		expenses=Yearly_expenses;
+
+	}
+
+}
